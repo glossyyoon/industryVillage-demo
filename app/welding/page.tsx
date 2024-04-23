2dc74731ddd2b5de63ced813e01051a6206c8e83
Jiyun Park <jiyunp@amazon.com>
2024-04-23 22:13:14 +0900

    ✨ UI 모듈화

--- app/welding/page.tsx
+++ app/welding/page.tsx
@@ -1,18 +1,32 @@
-import Link from 'next/link';
 import Image from 'next/image';
 import dynamic from 'next/dynamic';
-import '@/app/ui/global.css'
+import '@/app/ui/global.css';
+import ScoreDisplay from '../ui/components/ScoreDisplay';
+import React, {FC} from 'react';
 
 const WebcamComponent = dynamic(() => import('../ui/components/webcam'), {
     ssr: false
   });
+
+interface Props {
+    // any props that come into the component
+    name: String
+}
+
+const ConstructionItemComponent = dynamic(() => import('../ui/components/item'), {
+  ssr: false
+});
+
+const CheckComponent = dynamic(() => import('../ui/components/check'), {
+  ssr: false
+});
 
 export default function Page() {
     return (
       <main>
         <div className="relative w-full h-screen flex justify-center">
           <div><WebcamComponent /></div>
-          <div className="absolute z-1 left-5 top-5">
+          <div className="absolute z-2 left-10 mt-10">
                 <Image src="/construction.png"
                   width={200}
                   height={100}
@@ -20,13 +34,34 @@ export default function Page() {
                   alt="Screenshots of the dashboard project showing desktop version"
                 />
           </div>
-          <div className="absolute z-2 left-5 mt-100px">
-                <Image src="/const-문구.png"
-                  width={500}
-                  height={200}
-                  className="hidden md:block"
-                  alt="Screenshots of the dashboard project showing desktop version"
-                />
+          <div className="absolute z-2 left-10 text-instruction">
+                <p>용접 현장에서 사용해야하는 장비를 착용해주세요</p>
+          </div>
+          <div className="absolute z-2 right-10 text-score">
+            <ScoreDisplay />
+          </div>
+          <div className="absolute z-2 right-10 text-item-list"> 
+            <p>착용한 장비 목록</p>
+            <ConstructionItemComponent name={"안전모"} />
+            <CheckComponent />
+            <br />
+            <ConstructionItemComponent name={"조끼"} />
+            <CheckComponent />
+            <br />
+            <ConstructionItemComponent name={"장갑"} />
+            <CheckComponent />
+            <br />
+            <ConstructionItemComponent name={"고글"} />
+            <CheckComponent />
+            <br />
+            <ConstructionItemComponent name={"마스크"} />
+            <CheckComponent />
+            <br />
+          </div>
+          <br></br>
+          <div className="absolute z-2" style={{ display: 'flex', flexDirection: 'column', flexWrap: 'wrap'}}>
+          <div className="absolute z-2 right-4">
+          </div>
           </div>
         </div>
       </main>
